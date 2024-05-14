7b9c343e2cc1c32311b0d95566373e4073dfdb9c
Divya Bathala <S566466@nwmissouri.edu>
2024-05-14 00:53:23 -0500

    added all files

---
+++ Edu-learn/routes/users.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+let sum = 0;
+let more = 0;
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+
+  more++;
+  sum += more;
+
+  res.send(`Sum is: ${sum}`);
+  //res.send('respond with a resource');
+});
+
+
+module.exports = router;
